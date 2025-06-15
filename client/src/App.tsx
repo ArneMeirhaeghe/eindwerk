00d5c59ca49c23bdbf90b90d1bba21f00dd6a36d
ArneMeirhaeghe <126463424+ArneMeirhaeghe@users.noreply.github.com>
2025-06-15 18:37:00 +0200

    fix: Add ToastContainer to App component for improved notification display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import AppRoutes from "./routes/AppRoutes"
 import { AuthProvider } from "./context/AuthContext"
 import Sidebar from "./components/Sidebar"
+import { ToastContainer } from "react-toastify"
 
 function App() {
   return (
     <AuthProvider>
       <Sidebar />
       <AppRoutes />
+            <ToastContainer position="bottom-right" />
+
     </AuthProvider>
   )
 }
